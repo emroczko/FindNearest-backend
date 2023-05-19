1c9842a4820a0dd79844dd007d2ce999ba0113b3
Eryk <emroczko@me.com>
2023-05-20 00:43:24 +0200

    Added initial project files

---
+++ routingClient/src/main/java/spdb/routingclient/RoutingClientApplication.java
@@ -0,0 +1,13 @@
+package spdb.routingclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutingClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoutingClientApplication.class, args);
+    }
+
+}
---
+++ routingClient/src/test/java/spdb/routingclient/RoutingClientApplicationTests.java
@@ -0,0 +1,13 @@
+package spdb.routingclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutingClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
